daff5388889354550e75c874605d4d222f168d88
Wilson Gichinga <wilsong46@gmail.com>
2020-07-08 11:09:20 -0500

    Import HttpClientModule

--- ng-users/src/app/app.module.ts
+++ ng-users/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { UsersComponent } from './users/users.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
