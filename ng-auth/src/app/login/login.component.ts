0c326b858d776ef7ba564e8bf79eee13cc616e1d
Wilson Gichinga <wilsong46@gmail.com>
2020-07-08 09:47:32 -0500

    Create and Test

--- ng-auth/src/app/login/login.component.ts
+++ ng-auth/src/app/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+// 1. Import the service
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // 2. Inject the service into the constructor
+  constructor(private userService: UserService) {
 
-  ngOnInit(): void {
+    // 3. Call the test method from the service
+    console.log(this.userService.test());
   }
+
+  ngOnInit() {}
 
 }
