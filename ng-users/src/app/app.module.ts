8858ca8ee1b087f505ad2ee9e9287a7eb49a2bf7
Wilson Gichinga <wilsong46@gmail.com>
2020-07-08 11:32:56 -0500

    Generated and routed UserViewComponent

--- ng-users/src/app/app.module.ts
+++ ng-users/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UserViewComponent } from './user-view/user-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserViewComponent
   ],
   imports: [
     BrowserModule,
