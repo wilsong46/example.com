02317b2a3aac80868d61e76988c4ef279d1d320f
Wilson Gichinga <wilsong46@gmail.com>
2020-07-27 07:26:05 -0500

    react

--- ionicUsers/src/app/app-routing.module.ts
+++ ionicUsers/src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
   {
     path: 'users',
     loadChildren: () => import('./users/users.module').then( m => m.UsersPageModule)
+  },
+  {
+    path: 'user/:userId',
+    loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
   }
 ];
 
